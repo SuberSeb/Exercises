3fc83eb0114592eceaf3450036cdd45438508682
SuberSeb <suberseb@gmail.com>
2020-05-12 13:17:09 +0300

    Adding EF Core

---
+++ Exercise1/JsonImporter/JsonImporter/Database/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using JsonImporter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JsonImporter.Database
+{
+    class ApplicationDbContext : DbContext
+    {
+        public DbSet<Coach> Coaches { get; set; }
+        public DbSet<Detail> Details { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Team> Teams { get; set; }
+
+        public ApplicationDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=Messages;Username=postgres;Password=06090609");
+        }
+    }
+}
--- Exercise1/JsonImporter/JsonImporter/Program.cs
+++ Exercise1/JsonImporter/JsonImporter/Program.cs
@@ -1,5 +1,6 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
+using JsonImporter.Database;
 using JsonImporter.Json;
 using JsonImporter.Models;
 using JsonImporter.Tools;
@@ -8,30 +9,31 @@ using System.Collections.Generic;
 
 namespace JsonImporter
 {
-    public class Program
+    [SimpleJob(launchCount: 5)]
+    public class ParserBenchmark
     {
         private static readonly string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\message.json";
+        private readonly JsonParser jsonParser;
+        private readonly string jsonContent;
 
-        [SimpleJob(launchCount: 5)]
-        public class ParserBenchmark
+        public ParserBenchmark()
         {
-            private static readonly string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\message.json";
-            private readonly JsonParser jsonParser;
-            private readonly string jsonContent;
-
-            public ParserBenchmark()
-            {
-                jsonParser = new JsonParser();
-                jsonContent = Files.Read(path);
-            }
-
-            [Benchmark]
-            public List<Message> Parser() => jsonParser.Parse(jsonContent);
+            jsonParser = new JsonParser();
+            jsonContent = Files.Read(path);
         }
+
+        [Benchmark]
+        public List<Message> Parser() => jsonParser.Parse(jsonContent);
+    }
+
+    public class Program
+    {
+        private static readonly string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\message.json";        
 
         public static void Main(string[] args)
         {
-            int numberOfMessages = 500;
+            Console.Write("Enter a number of messages to generate (500 messages = 7.5 megabytes): ");
+            int numberOfMessages = Convert.ToInt32(Console.ReadLine());
 
             //JSON generation
             JsonGenerator jsonGenerator = new JsonGenerator();
@@ -40,8 +42,20 @@ namespace JsonImporter
             //Writing generated JSON in file
             Files.Write(path, json);
 
-            //Benchmark
-            var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+            Console.Write("Would you like to run benchmark for JSON parser? Y/N: ");
+            var selection = Console.ReadLine();
+            switch (selection)
+            {
+                //Benchmark
+                case "Y":
+                    BenchmarkRunner.Run(typeof(Program).Assembly);
+                    break;
+                case "N":
+                    break;
+                default:
+                    Console.WriteLine("Invalid input. Please try again.");
+                    break;
+            }
 
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
